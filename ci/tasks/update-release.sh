f7871a3fcc77123908c3d89efdc4c97f414307c3
Joshua Carp <jm.carp@gmail.com>
2016-06-24 09:59:51 -0400

    Fix docker repo.

--- ci/tasks/update-release.sh
+++ ci/tasks/update-release.sh
@@ -2,16 +2,18 @@
 
 set -e -u -x
 
+cd snort-release-source
+
 cat << EOF > config/private.yml
 $PRIVATE_YML
 EOF
 
 bosh -n sync blobs
-tar czvf snort-pulledpork-source/snort-conf.tar.gz snort-release-source/ci/conf/snort
+tar czvf snort-conf.tar.gz ci/conf/snort-conf
 
-if ! cmp snort-pulledpork-source/snort-conf.tar.gz blobs/snort-conf.tar.gz ; then
-  bosh -n add blob snort-pulledpork-source/snort-conf.tar.gz snort-conf.tar.gz
+if ! cmp snort-conf.tar.gz blobs/snort-conf.tar.gz ; then
+  bosh -n add blob snort-conf.tar.gz
   bosh -n upload blobs
   bosh -n create release --force --final --with-tarball
-  cp releases/snort/*.tgz finalized-release
+  cp releases/snort/*.tgz ../finalized-release
 fi
