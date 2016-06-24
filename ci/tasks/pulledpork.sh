d2c1c770b7baaf5bb4e824f76e9358631909bd2d
Joshua Carp <jm.carp@gmail.com>
2016-06-24 09:54:59 -0400

    Fix relative paths.

--- ci/tasks/pulledpork.sh
+++ ci/tasks/pulledpork.sh
@@ -2,8 +2,15 @@
 
 set -e -u -x
 
+DIR=$(pwd)
+
+cd snort-release-source/ci/config/snort-conf
+
 perl /opt/pulledpork-0.7.0/pulledpork.pl \
-  -c snort-release-source/ci/config/pulledpork.conf \
+  -c ../pulledpork.conf \
+  -S $SNORT_VERSION \
   -O $OINKCODE
+
+cd $DIR
 
 cp -r snort-release-source snort-pulledpork-source
