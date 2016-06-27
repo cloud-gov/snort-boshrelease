16c4f814aada88229f3beca24368c73cdbf28a75
Joshua Carp <jm.carp@gmail.com>
2016-06-27 11:41:30 -0400

    Compare tar checksums.

--- ci/tasks/update-release.sh
+++ ci/tasks/update-release.sh
@@ -9,9 +9,9 @@ $PRIVATE_YML
 EOF
 
 bosh -n sync blobs
-tar czvf snort-conf.tar.gz ci/conf/snort-conf
+tar czvf snort-conf.tar.gz ci/config/snort-conf
 
-if ! cmp snort-conf.tar.gz blobs/snort-conf.tar.gz ; then
+if [[ $(tar -xOf snort-conf.tar.gz | sha1sum) != $(tar -xOf blobs/snort-conf.tar.gz | sha1sum) ]] ; then
   bosh -n add blob snort-conf.tar.gz
   bosh -n create release --force --final --with-tarball
   cp releases/snort/*.tgz ../finalized-release
