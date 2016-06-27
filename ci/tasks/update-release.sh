dd3dbb01e161d7f483ebc77e4bf883b639baa8be
Joshua Carp <jm.carp@gmail.com>
2016-06-27 12:01:01 -0400

    Upload blobs before release.

--- ci/tasks/update-release.sh
+++ ci/tasks/update-release.sh
@@ -13,6 +13,7 @@ tar czvf snort-conf.tar.gz ci/config/snort-conf
 
 if [ "$(tar -xOf snort-conf.tar.gz | sha1sum)" != "$(tar -xOf blobs/snort-conf.tar.gz | sha1sum)" ] ; then
   bosh -n add blob snort-conf.tar.gz
+  bosh -n upload blobs
   bosh -n create release --force --final --with-tarball
   cp releases/snort/*.tgz ../finalized-release
 fi
