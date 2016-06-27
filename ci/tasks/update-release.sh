b73bbf1e2028890d80b369326183cd8a3febebc0
Joshua Carp <jm.carp@gmail.com>
2016-06-27 10:39:25 -0400

    Update blobstore config.

--- ci/tasks/update-release.sh
+++ ci/tasks/update-release.sh
@@ -13,7 +13,6 @@ tar czvf snort-conf.tar.gz ci/conf/snort-conf
 
 if ! cmp snort-conf.tar.gz blobs/snort-conf.tar.gz ; then
   bosh -n add blob snort-conf.tar.gz
-  bosh -n upload blobs
   bosh -n create release --force --final --with-tarball
   cp releases/snort/*.tgz ../finalized-release
 fi
